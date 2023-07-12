347a36fc4bb5b5ccecc4a7c55768e462526228a2
Samyak Jain <samjain06@gmail.com>
2023-07-12 01:56:37 -0500

    added more driver options

--- qa_bot.py
+++ qa_bot.py
@@ -24,10 +24,15 @@ def setup_selenium():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument('--disable-popup-blocking')
     chrome_options.add_argument('--disable-notifications')
     chrome_options.add_argument('--disable-infobars')
     chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument("--disable-features=NetworkService")
+    chrome_options.add_argument("--window-size=1920x1080")
+    chrome_options.add_argument("--disable-features=VizDisplayCompositor")
     driver = webdriver.Chrome(options=chrome_options)
     return driver
 
