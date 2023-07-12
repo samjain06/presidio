b10fd10323532f645e935f1c4b84d386a5c93687
Samyak Jain <samjain06@gmail.com>
2023-07-12 00:02:33 -0500

    Moved from flask to streamlit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# presidio
+# QUESTION-ANSWERING BOT
+
+## Deployment
+This app is deployed using Streamlit service at: [QABot](https://)
+
+## Introduction
+
+## Improvements
--- presidio.py
+++ presidio.py
@@ -1,83 +1,107 @@
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-import requests
+import re
 import time
-from transformers import pipeline
+import streamlit as st
 
-def search_web(topic):
+from concurrent.futures import ThreadPoolExecutor
+from bs4 import BeautifulSoup
+from googlesearch import search
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from transformers import pipeline
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def initialize_question_answering_pipeline():
+    return pipeline('question-answering', model='deepset/roberta-base-squad2')
+
+
+def normalize_context(context):
+    # Reconstruct the normalized context
+    normalized_context = re.sub(r'[\n\s]+', ' ', text)
+    return normalized_context
+
+
+def setup_selenium():
     # Set up Selenium
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')  # Run Chrome in headless mode (without opening a browser window)
-    chrome_options.add_argument('--disable-gpu')  # Disables the GPU usage (useful when running on a server without a GPU)
-    # disable popup
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--disable-popup-blocking')
-    # disable notifications
     chrome_options.add_argument('--disable-notifications')
-    # disable infobars
     chrome_options.add_argument('--disable-infobars')
-    # disable extensions
     chrome_options.add_argument('--disable-extensions')
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+    return driver
 
+
+def extract_info_from_link(link):
+    # Set up Selenium
+    driver = setup_selenium()
+
+    # Extract the information from the link
+    try:
+        driver.get(link)
+        time.sleep(3)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        text = soup.get_text()
+        cleaned_text = normalize_context(text)
+        return cleaned_text
+
+    except Exception as e:
+        print(f'[ERROR] encountered while extracting info from link {link}: {str(e)}')
+    finally:
+        driver.quit()
+
+
+def search_web(topic):
     # Search the web
-    topic = topic.replace(' ', '+')
     query = f'{topic}'
-    url = f'https://www.google.com/search?q={query}'
-    driver.get(url)
-
-    # Extract information from the search results
-    time.sleep(2)  # Wait for the page to load
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    # get all the links
-    search_results = soup.find_all('div', class_='yuRUbf')
+    search_results = list(search(query, num_results=10, lang='en'))
 
     # Extract the URLs from the search results
     info = []
-    for i in range(0, 2):
-        link = search_results[i].a.get('href')
-        driver.get(link)
-        time.sleep(5)  # Wait for the page to load due to dynamic content
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        text = soup.get_text().strip().replace('\n', ' ')
-        info.append({'link': link, 'snippet': text})
-
-    # Close the Selenium driver
-    driver.quit()
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(extract_info_from_link, link) for link in search_results]
+        for clean_text in futures:
+            try:
+                snippet = clean_text.result()
+                if snippet:
+                    info.append(snippet)
+            except Exception as e:
+                pass
 
     return info
 
 
-def initialize_question_answering_pipeline():
-    return pipeline('question-answering')
-
 def main():
     # Get the topic from the user
-    topic = input('Enter a topic: ')
+    st.title('Question Answering System')
+    st.header('Ask a question about a specific topic and Bot will answer it!')
+    topic = st.text_input('Type a topic you\'d like to learn more about', key='topic')
 
-    # Search the web and extract information
-    info = search_web(topic)
+    if topic != '' and topic is not None:
+        # Search the web and extract information
+        info = search_web(topic)
 
-    # Initialize the question-answering pipeline
-    qa_pipeline = initialize_question_answering_pipeline()
+        # Initialize the question-answering pipeline
+        qa_pipeline = initialize_question_answering_pipeline()
 
-    # Process follow-up questions
-    while True:
-        question = input('Enter a follow-up question (or type "quit" to exit): ')
-        if question.lower() == 'quit':
-            break
+        # Process follow-up questions
+        if info is not None:
+            question = st.text_input('Ask a follow-up question (or type "quit" to exit): ', key='follow-up-question')
+            if question != '' and question is not None:
+                if question.lower() == 'quit':
+                    st.stop()
 
-        # Concatenate all the snippets as the context for the question answering model
-        context = ' '.join([item['snippet'] for item in info])
-        print(context)
+                # Concatenate all the snippets as the context for the question answering model
+                context = ' '.join(info)
 
-        # Answer the question using the question-answering pipeline
-        answer = qa_pipeline(question=question, context=context)
-        print('Answer:', answer['answer'])
-        print('Score:', answer['score'])
-        print()
+                # Answer the question using the question-answering pipeline
+                answer = qa_pipeline(question=question, context=context)
+                st.write('Answer:', answer['answer'])
+                score = round(answer['score']*100, 2)
+                st.write('Score:', score)
+
 
 if __name__ == '__main__':
     main()
